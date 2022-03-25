804ca23ab5c98d2ba41625a81e4adb5a06fec0f0
alejocruzrcc <alejocruzzz@gmail.com>
2022-03-25 18:03:14 -0500

    intento root

--- treeproject/settings/pro.py
+++ treeproject/settings/pro.py
@@ -36,6 +36,7 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 
 STATIC_ROOT = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
 STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
+MEDIA_ROOT = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 
 
