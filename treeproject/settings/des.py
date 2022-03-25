9412f1b765114f48f7d4ad31232216f71a9999f0
alejocruzrcc <alejocruzzz@gmail.com>
2022-03-25 17:14:25 -0500

    de nuevo static

--- treeproject/settings/des.py
+++ treeproject/settings/des.py
@@ -7,7 +7,7 @@ DEBUG = os.environ.get('DEBUG', True)
 SECRET_KEY = env('SECRET_KEY')
 ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
-
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
 
