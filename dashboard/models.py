5e3aa871f6eecc111435c6c97bfd7db1bce40596
Luis Alejandro Cruz <alejocruzzz@gmail.com>
2023-04-06 00:21:27 -0500

    ahora se puede agregar commentario

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from accounts.models import Company
 
 # Create your models here.
+class CommentCompany(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved_comment = models.BooleanField(default=False)
+
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.body
